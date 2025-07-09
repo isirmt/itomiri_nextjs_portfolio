28d78b1dc693076c0ef2dc0459ffb327a6209b44
isirmt <r119306@g.kobe-kosen.ac.jp>
2025-07-10 00:40:12 +0900

    add: arrow overlay

--- src/components/common/welcomeOverlay.tsx
+++ src/components/common/welcomeOverlay.tsx
@@ -22,11 +22,18 @@ export default function WelcomeOverlay() {
     <div
       className={`
         fixed inset-0 z-[1000] flex items-center justify-center bg-white p-2
-        transform transition-transform duration-500 ease-in-out
-        ${sliding ? "translate-y-full" : "translate-y-0"}
+        w-full h-full
+        transform transition-transform duration-500 ease-in-out delay-100
+        ${sliding ? "translate-y-[200%]" : "translate-y-0"}
       `}
       onTransitionEnd={handleTransitionEnd}
     >
+      <svg className={`${sliding ? "bottom-[0%]" : "bottom-full"} absolute left-0 z-[1010] transform transition-all duration-500`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2056.17 103.47">
+        <polygon className="fill-teal-400" points="2056.17 56.76 1028.09 103.47 0 56.76 0 0 1028.09 46.7 2056.17 0 2056.17 56.76" />
+      </svg>
+      <svg className={`bottom-full absolute left-0 z-[1010] transform transition-all duration-200`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2056.17 103.47">
+        <polygon className="fill-teal-400" points="2056.17 56.76 1028.09 103.47 0 56.76 0 0 1028.09 46.7 2056.17 0 2056.17 56.76" />
+      </svg>
       <Player
         autoplay
         loop={false}
