b10dbfc85494f1ee2d8afa10f65fef4d568c28e5
isirmt <r119306@g.kobe-kosen.ac.jp>
2025-07-10 02:17:57 +0900

    add: click to skip, logo (short animation)

--- src/components/common/welcomeOverlay.tsx
+++ src/components/common/welcomeOverlay.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { Player } from "@lottiefiles/react-lottie-player";
-import itomiriLogo from "../../../public/itomiri_logo.json";
+import itomiriLogoShort from "../../../public/itomiri_logo_short.json";
 
 export default function WelcomeOverlay() {
   const [visible, setVisible] = useState(true);
@@ -21,29 +21,34 @@ export default function WelcomeOverlay() {
   return (
     <div
       className={`
-        fixed inset-0 z-[1000] flex items-center justify-center bg-white p-2
-        w-full h-full
-        transform transition-transform duration-500 ease-in-out delay-100
-        ${sliding ? "translate-y-[200%]" : "translate-y-0"}
+        fixed inset-0 z-[1000] flex flex-col gap-2 items-center justify-center bg-white p-2
+        w-full h-full cursor-pointer
+        transform transition-transform duration-200 ease-out
+        ${sliding ? "translate-y-[100%]" : "translate-y-0"}
       `}
       onTransitionEnd={handleTransitionEnd}
+      onClick={() => {
+        setTimeout(() => {
+          handleComplete();
+        }, 200)
+      }}
     >
-      <svg className={`${sliding ? "bottom-[0%]" : "bottom-full"} absolute left-0 z-[1010] transform transition-all duration-500`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2056.17 103.47">
-        <polygon className="fill-teal-400" points="2056.17 56.76 1028.09 103.47 0 56.76 0 0 1028.09 46.7 2056.17 0 2056.17 56.76" />
-      </svg>
-      <svg className={`bottom-full absolute left-0 z-[1010] transform transition-all duration-200`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2056.17 103.47">
-        <polygon className="fill-teal-400" points="2056.17 56.76 1028.09 103.47 0 56.76 0 0 1028.09 46.7 2056.17 0 2056.17 56.76" />
-      </svg>
       <Player
         autoplay
         loop={false}
-        src={itomiriLogo}
+        keepLastFrame
+        src={itomiriLogoShort}
         onEvent={(event) => {
           if (event === "complete") {
             handleComplete();
+          } else if (event === "play") {
+            setTimeout(() => {
+              handleComplete();
+            }, 1500)
           }
         }}
       />
+      <div className="absolute bottom-4 text-xl font-bold select-none text-teal-500 animate-pulse">Click to Skip</div>
     </div>
   );
 }
